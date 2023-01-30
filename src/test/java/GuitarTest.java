5431b347a06fdaa9c6463866637257616cda2cf6
Cordu7 <calendulagunst@hotmail.com>
2023-01-30 07:53:36 +0000

    added guitar test

--- src/test/java/GuitarTest.java
+++ src/test/java/GuitarTest.java
@@ -1,2 +1,41 @@
+import items.instruments.Cello;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+
+    Guitar guitar;
+    public void before() {
+        guitar = new Cello( "May", 28_000.00, 0.2, 6);
+    }
+
+    @Test
+    public void canGetMake(){
+        assertEquals("May", guitar.getMake());}
+    @Test
+    public void canGetPriceBought(){
+        assertEquals(28_000.00, guitar.getPriceBought(),0.0);}
+    @Test
+    public void canGetMarkup(){
+        assertEquals(0.2, guitar.getMarkup(), 0.0);}
+
+    @Test
+    public void canGetNumberOfStrings(){
+        assertEquals(6, guitar.getNumberOfStrings());
+    }
+
+
+    @Test
+    public void canPlay(){
+        assertEquals("pling", guitar.makesMusic());}
+    @Test
+    public void canGetSellingPrice(){
+        assertEquals(33_600, guitar.getSellingPrice(),0.0);}
+
+    @Test
+    public void canGetProfitMade(){
+        assertEquals(5600.00, guitar.getProfit(), 0.0);}
+
+
 }
