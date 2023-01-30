124d56af396156897513533d2ec89c1e3c74f7bc
Cordu7 <calendulagunst@hotmail.com>
2023-01-30 08:23:07 +0000

    added music stand test and musicStand

--- src/main/java/items/MusicStand.java
+++ src/main/java/items/MusicStand.java
@@ -1,4 +1,7 @@
 package items;
 
-public class MusicStand {
+public class MusicStand extends Item{
+    public MusicStand(String make, double priceBought, double markup) {
+        super(make, priceBought, markup);
+    }
 }
--- src/test/java/MusicStandTest.java
+++ src/test/java/MusicStandTest.java
@@ -1,2 +1,38 @@
+import items.DrumStick;
+import items.MusicStand;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MusicStandTest {
+
+    MusicStand musicStand;
+
+    @Before
+
+    public void before() {
+        musicStand = new MusicStand( "Grape", 10.00, 0.5);
+    }
+
+
+    @Test
+    public void canGetMake(){
+        assertEquals("Grape", musicStand.getMake());}
+    @Test
+    public void canGetPriceBought(){
+        assertEquals(10.00, musicStand.getPriceBought(),0.0);}
+    @Test
+    public void canGetMarkup(){
+        assertEquals(0.5, musicStand.getMarkup(), 0.0);}
+
+
+    @Test
+    public void canGetSellingPrice(){
+        assertEquals(15.00, musicStand.getSellingPrice(),0.0);}
+
+    @Test
+    public void canGetProfitMade(){
+        assertEquals(5.00, musicStand.getProfit(), 0.0);}
+
 }
